c1a7df5f7558ab7c8a5d0b9735971e22d4efe041
EhsanUllahKhan <ullahkhanihsan@yahoo.com>
2020-10-19 15:46:28 +0500

    Done for one request

--- server.js
+++ server.js
@@ -2,24 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
 const https = require('https');
+const { Body } = require("node-fetch");
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-    https.get('https://reqres.in/api/users?page=2', (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
+    // var data = "ad";
+    // https.get('https://reqres.in/api/users?page=2', (res1) => {
+    //     res1.on('data', (d) => {
+    //         data = d;
+    //         console.log(data);
+    //     });
 
-        res.on('data', (d) => {
-            process.stdout.write(d);
-        });
+    // }).on('error', (e) => {
+    //     console.error(e);
+    // });
 
-    }).on('error', (e) => {
-        console.error(e);
+    // res.send("<h1>ReqRes get API</h1>")
+    // console.log("Data is ", data);
+
+    const request = require('request');
+    request('https://reqres.in/api/users?page=2', function (error, response, body) {
+        var data = JSON.parse(body)
+        res.send(data["data"])
+        // console.log(data["data"])
     });
-
-    res.send("<h1>ReqRes get API</h1>")
 })
+
+
 
 app.get("/user/:id", (req, res) => {
     const accountId = Number(req.params.id);
